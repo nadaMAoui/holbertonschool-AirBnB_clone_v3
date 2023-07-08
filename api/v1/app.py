df2b42a9c2a3060b953c94a6c97d9d77a566f32d
nadaMAoui <maaouinada747@gmail.com>
2023-07-08 20:12:12 +0200

    >>>

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-create a variable app, instance of Flask
+This script creates a Flask application instance and configures it.
+
+It sets up the necessary components for the API, including routes, error handlers, and database connections.
 """
 
 from flask import Flask, jsonify
@@ -16,16 +18,31 @@ app.register_blueprint(app_views)
 
 @app.route("/")
 def hello():
-    return ("hello")
+    """
+    This route returns a simple "hello" message.
+
+    It is a test route to verify that the application is running correctly.
+    """
+    return "hello"
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """
+    This function is called when the application context is torn down.
+
+    It ensures that the database connection is properly closed to prevent resource leaks.
+    """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found_error(error):
+    """
+    This error handler is triggered when a 404 error occurs.
+
+    It returns a JSON response with an error message indicating that the requested resource was not found.
+    """
     return jsonify({"error": "Not found"}), 404
 
 
@@ -33,5 +50,4 @@ if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
-
 
